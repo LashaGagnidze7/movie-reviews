b31eeffa207703ad3f429182c0f9d91ec375b5b3
LashaGagnidze <lashadeveloper@gmail.com>
2023-03-10 20:48:41 +0400

    MOVIES VIEW WITH DUMMY DATA

--- frontend/src/services/ReviewService.js
+++ frontend/src/services/ReviewService.js
@@ -2,17 +2,14 @@ import axios from "axios";
 
 export default class ReviewService {
   static async createReview(data) {
-    const res = await axios.post('http://localhost:5000/api/v1/movies/review', data,);
-    return res;
+    return await axios.post('http://localhost:5000/api/v1/movies/review', data);
   }
 
   static async deleteReview(data) {
-    const res = await axios.delete('http://localhost:5000/api/v1/movies/review', {data});
-    return res;
+    return await axios.delete('http://localhost:5000/api/v1/movies/review', {data});
   }
 
   static async updateReview(data) {
-    const res = await axios.put('http://localhost:5000/api/v1/movies/review', data);
-    return res;
+    return await axios.put('http://localhost:5000/api/v1/movies/review', data);
   }
 }
