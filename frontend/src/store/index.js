5cdaacdd7458de68cbb8896b51476e686838d9f7
LashaGagnidze <lashadeveloper@gmail.com>
2023-03-11 20:54:47 +0400

    LOGIN SYSTEM

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,24 @@
-import { createStore } from 'vuex';
+import {createStore} from 'vuex';
 
 export default createStore({
   state: {
+    user: {
+      id: '',
+      name: '',
+    }
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    connect(state, data) {
+      // here you make an API call to authenticate user
+      state.user.id = data.id;
+      state.user.name = data.name;
+    },
+    disconnect(state) {
+      state.user.id = '';
+      state.user.name = '';
+    },
   },
-  actions: {
-  },
-  modules: {
-  },
+  actions: {},
+  modules: {},
 });
