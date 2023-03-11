dfcada0f02280a2b868925b8d98411813d27201c
LashaGagnidze <lashadeveloper@gmail.com>
2023-03-11 20:22:04 +0400

    LISTING REVIEWS

--- frontend/src/views/Movie.vue
+++ frontend/src/views/Movie.vue
@@ -7,37 +7,46 @@
           <img v-if="movie.poster"
                class="card-img-top" :src="movie.poster" alt=""/></div>
         <div class="col-md-6 col-sm-12">
-          <p class="card-text">{{ movie.plot }}</p>
+          <p class="card-text">{{ movie.fullplot }}</p>
           <div>
             Form to Add Reviews
           </div>
           <hr/>
-          <h3>Reviews</h3></div>
+          <h3>Reviews</h3>
+          <ul class="list-group">
+            <li class="list-group-item pb-3 pt-3" v-for="review in movie.reviews"
+                :key="review._id">
+              <h5 class="card-title">Review by {{ review.name }}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{{ getFormattedDate(review.date) }}</h6>
+              <p class="card-text">{{ review.review }}</p>
+              <a class="btn btn-primary me-3"> Edit </a>
+              <a class="btn btn-danger"> Delete </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import * as moment from 'moment';
 import MovieService from "@/services/MovieService";
 import {onMounted, ref} from "vue";
 import {useRoute} from "vue-router";
 
 const route = useRoute();
 
-const movie = ref({
-  poster: '',
-  title: '',
-  rated: '',
-  plot: '',
-  _id: '',
-  reviews: [],
-});
+const movie = ref({});
 
 const getMovie = async () => {
   movie.value = await MovieService.getMovie(
     route.params.id
   )
+};
+
+const getFormattedDate = (date) => {
+  return moment(date).format('Do MMMM YYYY');
 };
 
 onMounted(() => getMovie());
